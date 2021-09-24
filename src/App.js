48c502dce19787f009862cd038e512d990139a26
chrkglobal <rk.channagiri@gmail.com>
2021-09-24 15:55:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
-import React, {useState} from 'react';
-import './App.css';
+import React,{useState} from 'react'
 
 const App = () => {
-
-  const [wether, setWether] = useState();
-const [city, setCity] = useState("");
-const [result, setResult] = useState("");
-
-  const changeHandlar = (e) => {
-    console.log(city);
+  
+  const [city,setCity] = useState("");
+  const [result,setResult] = useState("");
+  const changeHandler = e =>{
     setCity(e.target.value);
   }
-
-  const submitWetherReport = (e) => {
+  const submitHandler = e =>{
     e.preventDefault();
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`)
-    .then( response=> response.json())
-    .then(data => {
-      // const kelvin = data.main.temp;
-      // const celcius = kelvin - 273.15;
-          setResult(
-            // "Temperature at "+city+"\n"+Math.round(celcius)+"°C"
-            data
-            );
-            setWether(data.weather[0]?.description);
-            console.log('12345', data, wether);
-    } )
-    setCity('');
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response=> response.json()).then(
+        data => {
+          const kelvin = data.main.temp;
+          const celcius = kelvin - 273.15;
+          setResult("Temperature at "+city+"\n"+Math.round(celcius)+"°C");
+        }
+      ).catch(error => console.log(error))
+      setCity("");
   }
 
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Wether Report App</h2>
-        <form onSubmit={submitWetherReport}>
-          <input type="text" value={city} onChange={changeHandlar} /><br />
-          <button type="submit">Get Wether Rport</button>
-        </form>
-        <div>{result.name}
-        {result.weather[0]?.description}
+    <div>
+      <center>
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Weather App</h4>
+            <form onSubmit={submitHandler}>
+              <input size="30" type="text" name="city" onChange={changeHandler} value={city}/> <br /><br />
+              <input type="submit" value="Get Temperature" />
+            </form><br /> <br />
+            <div>
+               <h1>{result}</h1> 
+            </div>
+          </div>
         </div>
-      </header>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
